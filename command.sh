3b88c59ffbd11600ef1d93b49630c00db72e44e9
Richard North <rich.north@gmail.com>
2018-11-20 07:31:12 +0000

    Improve logging

--- command.sh
+++ command.sh
@@ -2,39 +2,41 @@
 
 set -euo pipefail # Abort on error, strict variable interpolation, fail if piped command fails
 
-echo "dind-drone-plugin plugin"
+echo "📦 Starting dind-drone-plugin"
 
-echo "Starting docker-in-docker daemon"
+echo "🐳 Starting docker-in-docker daemon"
 /usr/local/bin/dockerd-entrypoint.sh dockerd --data-root /drone/docker -s overlay2 --log-level error &
 
 for i in $(seq 1 30); do
-  echo "Pinging docker daemon"
+  echo "⏳ Pinging docker daemon ($i/30)"
   docker ps &> /dev/null && break || true
   sleep 1
 done
 
 docker ps &> /dev/null || exit 1
-echo "Docker-in-Docker is running..."
+echo "✅ Docker-in-Docker is running..."
 
 if [[ "${PLUGIN_PREFETCH_IMAGES:-}" != "" ]]; then
-  echo "Prefetching images in background: ${PLUGIN_PREFETCH_IMAGES}"
+  echo "🚚 Prefetching images in background:"
   for IMG in $(echo ${PLUGIN_PREFETCH_IMAGES} | sed "s/,/ /g"); do
-    echo "Pulling prefetch image: $IMG"
+    echo "   $IMG"
     $(docker pull "$IMG" > /dev/null) &
   done
 fi
 
 cd ${CI_WORKSPACE}
 
-echo "Pulling build image: ${PLUGIN_BUILD_IMAGE}"
-docker pull ${PLUGIN_BUILD_IMAGE}
+echo "🚚 Pulling build image: ${PLUGIN_BUILD_IMAGE}"
+docker pull ${PLUGIN_BUILD_IMAGE} 2>&1 | sed 's/^/   /g'
 
 # Ensure that secrets (passed through as env vars) are available
 env > ${PWD}/outer_env_vars.env
 
 echo -e "\n\n"
-echo -e "About to run command: ${PLUGIN_CMD} inside docker image ${PLUGIN_BUILD_IMAGE}"
-echo -e "================================================================================\n\n"
+MSG="🚀 About to run command: ${PLUGIN_CMD} on image ${PLUGIN_BUILD_IMAGE} inside Docker-in-Docker"
+echo -e $MSG
+echo -n " $MSG" | sed 's/./=/g'
+echo -e "\n\n"
 
 CMD="docker run -v /var/run/docker.sock:/var/run/docker.sock \
                 -v ${PWD}:${PWD} -w ${PWD} --rm \
